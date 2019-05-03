273e6baa3b1da382c47a5a2abeb703d86c7e9376
Haley DeKeyser <hcd09@live.com>
2019-05-03 01:58:53 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,7 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot (words = "Squawk!")
-  puts words
-  words
+def parrot (phrase = "Squawk!")
+  puts phrase
+  phrase
 end
+
+parrot ("Pretty Bird!")
